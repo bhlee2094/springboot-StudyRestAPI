226893ef4f6b6adbb88d27eed218ed2d4ebc61ab
bh lee <djbh123@naver.com>
2021-03-25 16:03:42 +0900

    Study RestApi

---
+++ src/main/java/com/bhlee/startApp.java
@@ -0,0 +1,12 @@
+package com.bhlee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // 시작 설정 어노테이션
+public class startApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(startApp.class, args);
+    }
+}
